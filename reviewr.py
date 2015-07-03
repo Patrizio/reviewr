26af9b75572bc710e3a0e81b8fe9bce8519eab19
Patrizio <patrick.beeker@gmail.com>
2015-07-03 19:27:55 +0200

    Renamed guestbook and greeting to session and review

--- reviewr.py
+++ reviewr.py
@@ -10,28 +10,28 @@ from google.appengine.ext import ndb
 import webapp2
 
 
-class Greeting(ndb.Model):
-    """Models an individual Guestbook entry with author, content, and date."""
+class Review(ndb.Model):
+    """Models an individual Review entry with author, content, and date."""
     author = ndb.StringProperty()
     content = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 
-def guestbook_key(guestbook_name=None):
-    """Constructs a Datastore key for a Guestbook entity with guestbook_name"""
-    return ndb.Key('Guestbook', guestbook_name or 'default_guestbook')
+def session_key(session_name=None):
+    """Constructs a Datastore key for a Session entity with session_name"""
+    return ndb.Key('Session', session_name or 'default_session')
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.out.write('<html><body>')
-        guestbook_name = self.request.get('guestbook_name')
+        session_name = self.request.get('session_name')
 
         user = users.get_current_user()
 
-        greetings = self.get_greetings(guestbook_name)
+        reviews = self.get_reviews(session_name)
         
-        self.response.write(greetings)
+        self.response.write(reviews)
 
         if user:
             url = users.create_logout_url(self.request.uri)
@@ -43,83 +43,83 @@ class MainPage(webapp2.RequestHandler):
         self.response.write("""
           <form action="/sign?{}" method="post">
             <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Sign Guestbook"></div>
+            <div><input type="submit" value="Leave Review"></div>
           </form>
           <hr>
-          <form>Guestbook name: <input value="{}" name="guestbook_name">
+          <form>Session name: <input value="{}" name="session_name">
           <input type="submit" value="switch"></form>
 
         </body>
-      </html>""".format(urllib.urlencode({'guestbook_name': guestbook_name}),
-                        cgi.escape(guestbook_name)))
+      </html>""".format(urllib.urlencode({'session_name': session_name}),
+                        cgi.escape(session_name)))
 
-    def get_greetings(self, guestbook_name):
+    def get_reviews(self, session_name):
         """
-        get_greetings()
-        Checks the cache to see if there are cached greetings.
-        If not, call render_greetings and set the cache
+        get_reviews()
+        Checks the cache to see if there are cached reviews.
+        If not, call render_reviews and set the cache
 
         Args:
-          guestbook_name: Guestbook entity group key (string).
+          session_name: Session entity group key (string).
 
         Returns:
-          A string of HTML containing greetings.
+          A string of HTML containing reviews.
         """
-        greetings = memcache.get('{}:greetings'.format(guestbook_name))
-        if greetings is None:
-            greetings = self.render_greetings(guestbook_name)
-            if not memcache.add('{}:greetings'.format(guestbook_name),
-                                greetings, 10):
+        reviews = memcache.get('{}:reviews'.format(session_name))
+        if reviews is None:
+            reviews = self.render_reviews(session_name)
+            if not memcache.add('{}:reviews'.format(session_name),
+                                reviews, 10):
                 logging.error('Memcache set failed.')
-        return greetings
+        return reviews
 
-    def render_greetings(self, guestbook_name):
+    def render_reviews(self, session_name):
         """
-        render_greetings()
-        Queries the database for greetings, iterate through the
+        render_reviews()
+        Queries the database for reviews, iterate through the
         results and create the HTML.
 
         Args:
-          guestbook_name: Guestbook entity group key (string).
+          session_name: Session entity group key (string).
 
         Returns:
-          A string of HTML containing greetings
+          A string of HTML containing reviews
         """
-        greetings = ndb.gql('SELECT * '
-                            'FROM Greeting '
+        reviews = ndb.gql('SELECT * '
+                            'FROM Review '
                             'WHERE ANCESTOR IS :1 '
                             'ORDER BY date DESC LIMIT 10',
-                            guestbook_key(guestbook_name))
+                            session_key(session_name))
         output = cStringIO.StringIO()
-        for greeting in greetings:
-            if greeting.author:
-                output.write('<b>{}</b> wrote:'.format(greeting.author))
+        for review in reviews:
+            if review.author:
+                output.write('<b>{}</b> wrote:'.format(review.author))
             else:
                 output.write('An anonymous person wrote:')
             output.write('<blockquote>{}</blockquote>'.format(
-                cgi.escape(greeting.content)))
+                cgi.escape(review.content)))
         return output.getvalue()
 
 
-class Guestbook(webapp2.RequestHandler):
+class Session(webapp2.RequestHandler):
     def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each greeting
+        # We set the same parent key on the 'Review' to ensure each reviews
         # is in the same entity group. Queries across the single entity group
         # are strongly consistent. However, the write rate to a single entity
         # group is limited to ~1/second.
-        guestbook_name = self.request.get('guestbook_name')
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
+        session_name = self.request.get('session_name')
+        review = Review(parent=session_key(session_name))
 
         if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
+            review.author = users.get_current_user().nickname()
 
-        greeting.content = self.request.get('content')
-        greeting.put()
-        memcache.delete('{}:greetings'.format(guestbook_name))
+        review.content = self.request.get('content')
+        review.put()
+        memcache.delete('{}:reviews'.format(session_name))
         self.redirect('/?' +
-                      urllib.urlencode({'guestbook_name': guestbook_name}))
+                      urllib.urlencode({'session_name': session_name}))
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/sign', Guestbook)],
+                               ('/sign', Session)],
                               debug=True)
