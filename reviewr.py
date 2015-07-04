3e30df953f9de43151d8de12c7430351cde41024
Patrizio <patrick.beeker@gmail.com>
2015-07-04 10:26:58 +0200

    Added Jinja2 template and moved form there

--- reviewr.py
+++ reviewr.py
@@ -2,12 +2,19 @@ import cgi
 import cStringIO
 import logging
 import urllib
+import os
 
 from google.appengine.api import memcache
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
+import jinja2
 import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 class Review(ndb.Model):
@@ -40,18 +47,32 @@ class MainPage(webapp2.RequestHandler):
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
 
-        self.response.write("""
-          <form action="/sign?{}" method="post">
-            <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Leave Review"></div>
-          </form>
-          <hr>
-          <form>Session name: <input value="{}" name="session_name">
-          <input type="submit" value="switch"></form>
+      #   self.response.write("""
+      #     <form action="/sign?{}" method="post">
+      #       <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      #       <div><input type="submit" value="Leave Review"></div>
+      #     </form>
+      #     <hr>
+      #     <form>Session name: <input value="{}" name="session_name">
+      #     <input type="submit" value="switch"></form>
 
-        </body>
-      </html>""".format(urllib.urlencode({'session_name': session_name}),
-                        cgi.escape(session_name)))
+      #   </body>
+      # </html>""".format(urllib.urlencode({'session_name': session_name}),
+      #                   cgi.escape(session_name)))
+
+        template_values = {
+            'user': user,
+            'reviews': reviews,
+            'session_name': urllib.quote_plus(session_name),
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+
+
 
     def get_reviews(self, session_name):
         """
