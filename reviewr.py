9ca64f37689d10a8ecb4ba2fca897901195b196a
Patrizio <patrick.beeker@gmail.com>
2015-07-05 16:00:59 +0200

    Render reviews from datamodel

--- reviewr.py
+++ reviewr.py
@@ -31,14 +31,13 @@ def session_key(session_name=None):
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('<html><body>')
         session_name = self.request.get('session_name')
 
         user = users.get_current_user()
 
         reviews = self.get_reviews(session_name)
         
-        self.response.write(reviews)
+        #self.response.write(reviews)
 
         if user:
             url = users.create_logout_url(self.request.uri)
@@ -46,19 +45,6 @@ class MainPage(webapp2.RequestHandler):
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
-
-      #   self.response.write("""
-      #     <form action="/sign?{}" method="post">
-      #       <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      #       <div><input type="submit" value="Leave Review"></div>
-      #     </form>
-      #     <hr>
-      #     <form>Session name: <input value="{}" name="session_name">
-      #     <input type="submit" value="switch"></form>
-
-      #   </body>
-      # </html>""".format(urllib.urlencode({'session_name': session_name}),
-      #                   cgi.escape(session_name)))
 
         template_values = {
             'user': user,
@@ -71,9 +57,6 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
-
-
-
     def get_reviews(self, session_name):
         """
         get_reviews()
@@ -84,42 +67,32 @@ class MainPage(webapp2.RequestHandler):
           session_name: Session entity group key (string).
 
         Returns:
-          A string of HTML containing reviews.
+          Collection of reviews.
         """
         reviews = memcache.get('{}:reviews'.format(session_name))
         if reviews is None:
             reviews = self.render_reviews(session_name)
             if not memcache.add('{}:reviews'.format(session_name),
-                                reviews, 10):
+                                reviews, 20):
                 logging.error('Memcache set failed.')
         return reviews
 
     def render_reviews(self, session_name):
         """
         render_reviews()
-        Queries the database for reviews, iterate through the
-        results and create the HTML.
+        Queries the database for reviews.
 
         Args:
           session_name: Session entity group key (string).
 
         Returns:
-          A string of HTML containing reviews
+          Collection of reviews.
         """
-        reviews = ndb.gql('SELECT * '
-                            'FROM Review '
-                            'WHERE ANCESTOR IS :1 '
-                            'ORDER BY date DESC LIMIT 10',
-                            session_key(session_name))
-        output = cStringIO.StringIO()
-        for review in reviews:
-            if review.author:
-                output.write('<b>{}</b> wrote:'.format(review.author))
-            else:
-                output.write('An anonymous person wrote:')
-            output.write('<blockquote>{}</blockquote>'.format(
-                cgi.escape(review.content)))
-        return output.getvalue()
+       
+        reviews_query = Review.query(ancestor=session_key(session_name)).order(-Review.date)
+        reviews = reviews_query.fetch(20)
+
+        return reviews
 
 
 class Session(webapp2.RequestHandler):
